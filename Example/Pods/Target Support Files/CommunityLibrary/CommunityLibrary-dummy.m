fcad34cc0e55b650c9a2f97b7d5d2e1e2c61c350
Aleksandr Vnukov <aleksandr.vnukov.jos@gmail.com>
2016-07-30 15:06:44 +0300

    update pod.

--- Example/Pods/Target Support Files/CommunityLibrary/CommunityLibrary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CommunityLibrary : NSObject
-@end
-@implementation PodsDummy_CommunityLibrary
-@end
--- Example/Pods/Target Support Files/Pods-CommunityLibrary_Tests/Pods-CommunityLibrary_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CommunityLibrary_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CommunityLibrary_TestsVersionString[];
-
